b18a298350fe1c13caae5d7dd0fc660c39417069
oluwatoyeidowu <oagbonjinmi@gmail.com>
2022-11-11 05:35:25 -0800

    H Fix

--- sort.h
+++ sort.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "sort.h"
 
 /*Comparison direction macros for bitonic sort*/
 #define UP 0
@@ -42,7 +43,14 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
 
 
 #endif
